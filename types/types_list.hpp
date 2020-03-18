fd47ad512890d8b7e526a1f92b15ec22a06d40de
Gilmar Custodio de Sales <gilmar@MacBook-Pro-de-Gilmar.local>
2020-03-18 10:35:09 -0300

    add static iteration by vittorio romeo and type_list

---
+++ types/types_list.hpp
@@ -0,0 +1,127 @@
+#pragma once
+
+#include <utility>
+#include <tuple>
+
+#include "../iteration/static_for.hpp"
+
+namespace mp {
+    namespace impl {
+
+        //type_list
+        template<typename...Ts>
+        struct type_list;
+
+        template<typename T>
+        struct type_list<T> {
+            using type = T;
+            static constexpr int index = 0;
+        };
+
+        template<typename T, typename... Ts>
+        struct type_list<T, Ts...> : type_list<Ts...>  {
+            using type = T;
+            static constexpr int index = type_list<Ts...>::index + 1;
+        };
+
+        //tuple index_of 
+        template <class T, class Tuple>
+        struct index_of;
+
+        template <typename T>
+        struct index_of<T, std::tuple<>> {
+            static constexpr std::size_t value = -1;
+        };
+
+        template <class T, class... Ts>
+        struct index_of<T, std::tuple<T, Ts...>> {
+            static constexpr std::size_t value = 0;
+        };
+
+        template <class T, class U, class... Ts>
+        struct index_of<T, std::tuple<U, Ts...>> {
+            static constexpr std::size_t value = 1 + index_of<T, std::tuple<Ts...>>::value;
+        };
+
+        //tuple size_of
+
+        template<typename...Ts>
+        struct size_of;
+
+        template<typename T>
+        struct size_of<std::tuple<T>> {
+            static constexpr int value = 1;
+        };
+
+        template<typename T, typename... Ts>
+        struct size_of<std::tuple<T, Ts...>> : size_of<std::tuple<Ts...>>  {
+            static constexpr int value = size_of<std::tuple<Ts...>>::value + 1;
+        };
+
+        //tuple contains
+        template <typename T, typename Tuple>
+        struct contains;
+
+        template <typename T>
+        struct contains<T, std::tuple<>> : std::false_type {};
+
+        template <typename T, typename U, typename... Ts>
+        struct contains<T, std::tuple<U, Ts...>> : contains<T, std::tuple<Ts...>> {};
+
+        template <typename T, typename... Ts>
+        struct contains<T, std::tuple<T, Ts...>> : std::true_type {};
+
+
+        //make a type_list be a tuple
+        template<class List> struct as_tuple;
+
+        template<template<class...> class List, class... Elements>
+        struct as_tuple<List<Elements...>> {
+            using type = std::tuple<Elements...>;
+        };
+
+
+        //unwrap a list to another container
+        template<template<class...> class Container, class List>
+        struct rename;
+
+        template<template<class...> class Container,
+            template<class...> class List,
+            class... Elements
+        >
+        struct rename<Container, List<Elements...>> { using type = Container<Elements...>; };
+
+    }
+
+    template<typename... Ts>
+    using type_list = impl::type_list<Ts...>;
+
+    template<template<class...> class Container, class List>
+    using rename = typename impl::rename<Container, List>::type;
+
+    template <typename T, typename Tuple>
+    using contains = typename impl::contains<T, Tuple>::type;
+
+    template<typename T, typename Tuple>
+    constexpr size_t index_of(Tuple) {
+        static_assert(contains<T, Tuple>{}, "invalid type");
+        return impl::index_of<T, Tuple>::value;
+    }
+
+    template<typename Tuple>
+    constexpr size_t size_of(Tuple) {
+        return impl::size_of<Tuple>::value;
+    }
+
+    template <typename TF, typename TTuple>
+    void for_tuple(TF&& f, TTuple&& t)
+    {
+        auto adapted = [f = FWD(f)](auto&&... xs)
+        {
+            for_args(f, FWD(xs)...);
+        };
+
+        std::apply(adapted, FWD(t));
+    }
+
+}
